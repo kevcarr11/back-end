8c2007fdc86234a422f144d2c984d5902a36be13
Matt Hagner <web@matthagner.com>
2019-09-25 23:03:15 -0500

    Implement categories controller

--- src/controllers/categories.js
+++ src/controllers/categories.js
@@ -9,6 +9,51 @@ async function create(input) {
   return category ? category : null;
 }
 
+function getAll() {
+  return db.from("categories").select("*");
+}
+
+async function getByField(fieldName, value) {
+  const [category] = await db
+    .from("categories")
+    .select("*")
+    .where({ [fieldName]: value });
+
+  return category ? category : null;
+}
+
+function getById(id) {
+  return getByField("id", id);
+}
+
+function getByName(name) {
+  return getByField("name", name);
+}
+
+async function updateById(id, update) {
+  const [category] = await db
+    .from("categories")
+    .update(update)
+    .where({ id })
+    .returning("*");
+
+  return category ? category : null;
+}
+
+async function deleteById(id) {
+  const count = await db
+    .from("categories")
+    .del()
+    .where({ id });
+
+  return count === 1;
+}
+
 module.exports = {
-  create
+  create,
+  getAll,
+  getById,
+  getByName,
+  updateById,
+  deleteById
 };
--- src/controllers/categories.test.js
+++ src/controllers/categories.test.js
@@ -15,11 +15,58 @@ it("should create a category", async () => {
   const categories = await db.from("categories").select("*");
 
   expect(categories.length).toBe(1);
-  expect(categories[0].name).toBe("New Category");
+  expect(categories[0].name).toBe(input.name);
 });
 
-it.skip("should get all categories", async () => {});
+it("should return the category when created", async () => {
+  const input = { name: "new category" };
+  const category = await Categories.create(input);
 
-it.skip("should update a category", async () => {});
+  expect(category.name).toBe(input.name);
+});
 
-it.skip("should delete a category", async () => {});
+it("should get all categories", async () => {
+  await db.from("categories").insert({ name: "cat-1" });
+  await db.from("categories").insert({ name: "cat-2" });
+
+  const categories = await Categories.getAll();
+  const [cat1, cat2] = categories;
+
+  expect(categories.length).toBe(2);
+  expect(cat1.name).toBe("cat-1");
+  expect(cat2.name).toBe("cat-2");
+});
+
+it("should get category by id", async () => {
+  await db.from("categories").insert({ name: "cat-1" });
+
+  const category = await Categories.getById(1);
+
+  expect(category.name).toBe("cat-1");
+});
+
+it("should get category by name", async () => {
+  await db.from("categories").insert({ name: "cat-1" });
+
+  const category = await Categories.getByName("cat-1");
+
+  expect(category.id).toBe(1);
+});
+
+it("should update a category", async () => {
+  await db.from("categories").insert({ name: "cat-1" });
+
+  const category = await Categories.updateById(1, { name: "test" });
+
+  expect(category.name).toBe("test");
+});
+
+it("should delete a category", async () => {
+  await db.from("categories").insert({ name: "cat-1" });
+
+  const deleted = await Categories.deleteById(1);
+  const categories = await db.from("categories").select("*");
+
+  expect(deleted).toBe(true);
+  expect(categories.length).toBe(0);
+});
